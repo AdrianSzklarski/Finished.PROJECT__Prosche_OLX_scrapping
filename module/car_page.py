728a8c7477a3043dcc98c06ff92fcd397ac4050d
AdrianSzklarski <adrian_it@o2.pl>
2023-01-28 15:52:34 +0100

    Added set window

--- module/car_page.py
+++ module/car_page.py
@@ -2,3 +2,11 @@ class Page:
     def __init__(self, root):
         self.root = root
 
+        # Calling up methods
+        self.get_set_window()
+
+    def get_set_window(self):
+        ''' Main window settings '''
+        self.root.title('Porsche Cars, Program Created by Adrian Szklarski, 01.2023')
+        self.root.wm_attributes('-zoomed', True)
+        self.root.resizable(width=False, height=False)
